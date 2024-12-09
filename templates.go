2d4139c729d0928464c64ac53b4e930bdeea1f29
Jan Galek <admin@gcore.cz>
2024-12-09 08:28:49 +0100

    fixup! Added Diago

--- templates.go
+++ templates.go
@@ -10,28 +10,10 @@ var DiagoPanelTemplate string
 //go:embed templates/latency/diago_latency_panel.gohtml
 var DiagoLatencyPanelTemplate string
 
-//go:embed templates/route/diago_route_panel.gohtml
-var DiagoRoutePanelTemplate string
-
-//go:embed templates/route/diago_route_panel_js.gohtml
-var DiagoRoutePanelJSTemplate string
-
-//go:embed templates/route/diago_route_panel_popup.gohtml
-var DiagoRoutePanelPopupTemplate string
-
 func GetDiagoPanelTemplate() string {
 	return DiagoPanelTemplate
 }
 
 func GetDiagoLatencyPanelTemplate() string {
 	return DiagoLatencyPanelTemplate
-}
-func GetDiagoRoutePanelTemplate() string {
-	return DiagoRoutePanelTemplate
-}
-func GetDiagoRoutePanelJSTemplate() string {
-	return DiagoRoutePanelJSTemplate
-}
-func GetDiagoRoutePanelPopupTemplate() string {
-	return DiagoRoutePanelPopupTemplate
 }

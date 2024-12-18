178ea9b1816ca71b175d23658a68eaacb3eb3c47
Jan Galek <admin@gcore.cz>
2024-12-18 02:58:52 +0100

    [Test] cover release mode by test

--- tests/diagoMiddleware_test.go
+++ tests/diagoMiddleware_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gouef/diago"
+	"github.com/gouef/router"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -51,4 +52,31 @@ func TestDiagoMiddleware(t *testing.T) {
 	assert.Contains(t, w.Body.String(), "<div>Panel</div>")
 	assert.Contains(t, w.Body.String(), "<div>Content</div>")
 	assert.Contains(t, w.Body.String(), "<script>console.log('JS');</script>")
+}
+
+func TestDiagoMiddlewareReleaseMode(t *testing.T) {
+	r := router.NewRouter()
+	r.EnableRelease()
+	n := r.GetNativeRouter()
+	gin.SetMode(gin.ReleaseMode)
+
+	d := diago.NewDiago()
+
+	d.AddExtension(&MyExtension{})
+
+	n.GET("/test", diago.DiagoMiddleware(r, d), func(c *gin.Context) {
+		c.Header("Content-Type", "text/html; charset=utf-8")
+		c.String(200, "Hello, world!")
+	})
+	w := httptest.NewRecorder()
+	req, err := http.NewRequest("GET", "/test", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	n.ServeHTTP(w, req)
+
+	assert.Equal(t, "text/html; charset=utf-8", w.Header().Get("Content-Type"))
+
+	assert.Equal(t, w.Body.String(), "Hello, world!")
 }
